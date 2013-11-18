448df73f13010ee3ca4212997e387884f0de12ac
phillc <spyyderz@gmail.com>
2013-11-18 18:08:31 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fantasyplayoffs::Application.config.secret_key_base = 'b137841961b218ed45680a6e82878221cf5d148a4576baf81316df0bdaf354d5225493197c33e635292cb28abe453be2a83e5e0456546551d42ee1ddb13c0f67'
