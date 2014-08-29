5da94099964adb2201416ee791668068e996bd57
phillc <spyyderz@gmail.com>
2014-08-29 07:27:23 -0400

    Draft board

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     authorize :welcome, :index?
+    @leagues = League
+                 .where.not(synced_at: nil)
+                 .limit(10)
+                 .order("RANDOM()")
   end
 end
