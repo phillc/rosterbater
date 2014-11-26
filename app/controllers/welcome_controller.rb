7b07a4b70a6087a3761462fa578fe788b12a97fa
phillc <spyyderz@gmail.com>
2014-11-26 17:47:21 -0500

    Cache busting for realz

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
   def index
     authorize :welcome, :index?
     @draft_leagues = League.interesting_draft.limit(10)
-    @season_leagues = League.interesting_season.limit(10)
-    ActiveRecord::Base.connection.clear_query_cache
-    @season_leagues2 = League.interesting_season.limit(10)
+    @season_leagues = League.interesting_season.limit(10).to_a
+    League.connection.clear_query_cache
+    @season_leagues2 = League.interesting_season.limit(10).to_a
   end
 end
