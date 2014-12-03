72fc261ee346c4a963f5444bf604c928ee82684f
phillc <spyyderz@gmail.com>
2014-12-03 08:57:35 -0500

    Messing with ads

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,25 @@ module ApplicationHelper
     end
   end
 
+  def footer_ad
+    number = rand(1..3)
+    if number == 1
+      render "shared/footer_ad"
+    else
+      content_tag :div, class: "text-center" do
+        render "shared/amazon_banner_ad_#{number}"
+      end
+    end
+  end
+
   def large_banner_ad
-    content_tag :div, class: "text-center" do
-      render "layouts/footer_ad_#{rand(2..3)}"
+    number = rand(0..3)
+    if number == 0 || number == 1
+      render "shared/google_large_banner_ad"
+    else
+      content_tag :div, class: "text-center" do
+        render "shared/amazon_banner_ad_#{rand(2..3)}"
+      end
     end
   end
 end
