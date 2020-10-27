8f635a00643bc749d3c282d0bc49178839385648
phillc <15082+phillc@users.noreply.github.com>
2020-10-27 16:35:20 -0400

    Remove all ads and social

--- server/app/helpers/application_helper.rb
+++ server/app/helpers/application_helper.rb
@@ -5,28 +5,6 @@ module ApplicationHelper
     end
   end
 
-  def footer_ad
-    number = rand(1..3)
-    if number == 1
-      render "shared/footer_ad"
-    else
-      content_tag :div, class: "text-center" do
-        render "shared/amazon_banner_ad_#{number}"
-      end
-    end
-  end
-
-  def large_banner_ad
-    number = rand(0..3)
-    content_tag :div, class: "text-center" do
-      if number == 0 || number == 1
-        render "shared/google_large_banner_ad"
-      else
-        render "shared/amazon_banner_ad_#{rand(2..3)}"
-      end
-    end
-  end
-
   def week_1_alert
     if @league.start_week != 1
       content_tag :div, class: "alert alert-danger" do
