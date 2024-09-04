1356aeefa619b440d8d367e7232066f7badf2661
Phillip Campbell <15082+phillc@users.noreply.github.com>
2024-09-04 19:07:05 -0400

    Cert update

--- cert.sh
+++ cert.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -ex
 # sudo certbot certonly --key-type rsa --manual --preferred-challenges dns
-sudo kubectl delete secret rosterbater-tls
+kubectl delete secret rosterbater-tls
 sudo kubectl create secret tls rosterbater-tls --cert=/etc/letsencrypt/live/rosterbater.com/fullchain.pem --key=/etc/letsencrypt/live/rosterbater.com/privkey.pem
+kubectl delete service rosterbater-load-balancer
+./apply.sh
