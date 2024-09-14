8c42ca55001c7beb76f3e87b2b8e900ee3386f6a
Phillip Campbell <15082+phillc@users.noreply.github.com>
2024-09-14 11:23:48 -0400

    Getting things working again

--- server/config/puma.rb
+++ server/config/puma.rb
@@ -5,17 +5,3 @@ preload_app!
 
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
-
-on_worker_boot do
-  # worker specific setup
-  ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-                Rails.application.config.database_configuration[Rails.env]
-    # config['pool'] = ENV['MAX_THREADS'] || 16
-    ActiveRecord::Base.establish_connection(config)
-  end
-
-  if defined?(Resque)
-     Resque.redis = ENV["<redis-uri>"] || "redis://127.0.0.1:6379"
-  end
-end
